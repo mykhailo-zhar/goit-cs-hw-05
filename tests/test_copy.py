a9fb7d669fd19c3a9384d50d23a0983e007b833a
Mykhailo Zhar <mykhailo.zhar.od.ua@gmail.com>
2026-09-17 12:27:42 +0300

    Add async tests

--- tests/test_copy.py
+++ tests/test_copy.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import pytest
 
-from src.file_copy import copy
+from src.file_copy import copy_async
 
 
 def create_source_dir(tmp_dir: Path) -> Path:
@@ -36,36 +36,44 @@ def count_files(dir_path: Path, extension: str) -> int:
 
 
 class TestCopy:
-    def test_raises_when_source_and_destination_absent(self) -> None:
+    @pytest.mark.asyncio
+    async def test_raises_when_source_and_destination_absent(self) -> None:
         with pytest.raises(TypeError):
-            copy()  # type: ignore[call-arg]
+            await copy_async()  # type: ignore[call-arg]
 
-    def test_raises_when_source_is_a_file(self, tmp_path: Path) -> None:
+    @pytest.mark.asyncio
+    async def test_raises_when_source_is_a_file(self, tmp_path: Path) -> None:
         temp_file = create_file(tmp_path, "test.txt")
         with pytest.raises(ValueError):
-            copy(temp_file, "./dist")
+            await copy_async(temp_file, "./dist")
 
-    def test_raises_when_source_directory_does_not_exist(self) -> None:
+    @pytest.mark.asyncio
+    async def test_raises_when_source_directory_does_not_exist(self) -> None:
         with pytest.raises(ValueError):
-            copy("not_a_directory", "./dist")
+            await copy_async("not_a_directory", "./dist")
 
-    def test_raises_when_destination_is_a_file(self, tmp_path: Path) -> None:
+    @pytest.mark.asyncio
+    async def test_raises_when_destination_is_a_file(self, tmp_path: Path) -> None:
         source_dir = create_source_dir(tmp_path)
         destination_file = create_file(tmp_path, "destination.txt")
         with pytest.raises(ValueError):
-            copy(source_dir, destination_file)
+            await copy_async(source_dir, destination_file)
 
-    def test_copies_one_file_under_same_extension(self, tmp_path: Path) -> None:
+    @pytest.mark.asyncio
+    async def test_copies_one_file_under_same_extension(self, tmp_path: Path) -> None:
         source_dir = create_source_dir(tmp_path)
         destination_dir = create_destination_dir(tmp_path)
         file_name = "test.txt"
         create_file(source_dir, file_name)
 
-        copy(source_dir, destination_dir)
+        await copy_async(source_dir, destination_dir)
 
         assert get_file_extname_dir(destination_dir, file_name).is_dir()
 
-    def test_copies_multiple_files_under_same_extension(self, tmp_path: Path) -> None:
+    @pytest.mark.asyncio
+    async def test_copies_multiple_files_under_same_extension(
+        self, tmp_path: Path
+    ) -> None:
         source_dir = create_source_dir(tmp_path)
         destination_dir = create_destination_dir(tmp_path)
         a_dir = source_dir / "a"
@@ -77,7 +85,7 @@ class TestCopy:
         create_file(a_dir, "test.js")
         create_file(b_dir, "test.js")
 
-        copy(source_dir, destination_dir)
+        await copy_async(source_dir, destination_dir)
 
         assert count_files(destination_dir, ".js") == 2
         assert count_files(destination_dir, ".txt") == 2
