b760765b9b685689c62f404b09913223a6c57ae4
Mykhailo Zhar <mykhailo.zhar.od.ua@gmail.com>
2026-09-18 11:29:17 +0300

    Change logname

--- src/utility.py
+++ src/utility.py
@@ -57,7 +57,7 @@ def get_filehandler(formatter: logging.Formatter, prefix):
 
     tz = timezone(timedelta(hours=2))  # GMT+2
     timestamp = datetime.now(tz=tz).strftime("%Y%m%d_%H%M%S")
-    log_filename = logs_dir / f"{prefix}_search_{timestamp}.log"
+    log_filename = logs_dir / f"{prefix}_copying_{timestamp}.log"
     file_handler = logging.FileHandler(log_filename)
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(formatter)
